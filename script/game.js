d9b4d697fdf966d1a851c3d1c44ad86ec2503033
roush55 <ranaabumelha@gmail.com>
2019-12-02 22:33:37 +0300

    step2

--- script/game.js
+++ script/game.js
@@ -22,8 +22,8 @@ setInterval(function(){
      const heightRandom =  Math.round(Math.random() * -100) - 100
     
      //widthRandom
-        const widthWindow=$(window).width()
-        const widthRandom=Math.round(Math.random()* (widthWindow-20 ))
+        const widthWindow=$('#contanier').width()
+        const widthRandom=Math.round(Math.random()* (widthWindow-10 ))
         // console.log(widthRandom)
         if (parseInt(element.style.top) > heightWindow){
             $(element).css({
@@ -32,39 +32,60 @@ setInterval(function(){
             })
 
         }else{
-            apple.animate({"top":"+=100"},"fast")
+            apple.animate({"top":"+=80"},"fast")
+        }
+        if( collision(element,$('#boy')) === true ){
+            console.log('touched it (: ')
         }
     })
 
-  },1000)
-//   const boy=$('.boy');
 
-//   function moveLeft(){
-//     boy.style.left=parseInt(boy.style.left)-5 +'px';
+  },300)
 
-// function moveRight(){
-//     boy.style.left=parseInt(boy.style.right)+5 +'px';
-// }
-
-// function moveSelection(){
-//     evt = evt || window.event; 
-//     switch (evt.keyCode){
-//         case 37: //left arrow key
-//             moveLeft();
-//             break;
-       
-//         case 39: //right arrow key
-//             moveRight();
-//             break;
-//         }				
-//     };
 const playBoy=$('.boy');
 $(document).keydown (function(e){ 
-    left=$(window).left
-    right=$(window).right
+
+    left=$(window).width()
+
     if (e.which == '37'){
         
-    playBoy.finish().animate({"left":"-=50"},"faster")}
+    playBoy.finish().animate({"left":"+=50"},100)}
     else if (e.which == '39'){
-    playBoy.finish().animate({"left":"+=50"},"faster")}
+    playBoy.finish().animate({"left":"-=50"},100)}
+    // if($(boyT).position())
+        // console.log($(".boy").position())
+        // console.log($(".apple").eq(0))
  })
+
+// function collision START HERE
+function collision(firstElment,secondElement){
+    // console.log('collision starting...')
+    // First element
+    let x1 = $(firstElment).offset().left
+    let y1 = $(firstElment).offset().top
+    let h1 = $(firstElment).outerHeight(true);
+    let w1 = $(firstElment).outerWidth(true);
+    let b1 = y1 + h1;
+    let r1 = x1 + w1;
+    // Second element
+    let x2 = $(secondElement).offset().left
+    let y2 = $(secondElement).offset().top
+    let h2 = $(secondElement).outerHeight(true);
+    let w2 = $(secondElement).outerWidth(true);
+    let b2 = y2 + h2;
+    let r2 = x2 + w2;
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2){
+        return false
+    }
+
+    return true;
+}
+
+const fElement = $('#apple1')
+const sElement = $('#boy')
+collision(fElement,sElement)
+// function collision END HERE
+
+
+
+
