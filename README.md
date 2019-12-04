bfe39d9cb9aa57d2c306291094416798ed27f18d
roush55 <ranaabumelha@gmail.com>
2019-12-04 20:12:09 +0300

    add project

---
+++ README.md
@@ -0,0 +1,32 @@
+# Project Title
+Catch Apple 
+
+Description
+The project is a game suitable for one player. It is inspired by drop and catch games.
+
+# Technical Used
+for this project I'v used :
+
+1-HTML
+2-CSS
+3-javascript 
+4-jQuery for DOM manipulation
+---
+![wireframe](path)
+# User Stories
+Planning and Development Process
+search for new meth that fits 
+I started the project with the layout first. I then specified the entities (the basic ones) that would set the skeleton of the program. Then, I styled the entities. Finally, I added some features that would make the game more appealing.
+---
+# Problem-Solving Strategy:
+1-write down the problem try to analysis.
+2-Asking and constant modification to the code.
+3-search && read other codes 
+ 
+
+# Unsolved problems:
+add more obstacle to the game and more levels.
+
+# References
+https://jquery.com
+https://codepen.io
--- script/game.js
+++ script/game.js
@@ -1,90 +1,141 @@
+let sec =15;
+let point = 0
+$(function(){
+ let start = false;
 
-// function myM(){
+   const startGame = function(){ //THE GAME START HERE
+        setTimeout(function(){
+            if(point >= 8){
+                alert('You Won (: ')      
+            }
+            else {
+                alert("Try again ): ")
+            }
+        }, sec*1000 + 1000)//THIS FUNCTION WILL RUN AFTER THE TIME FINISH 
 
-//     $('#apple1').sildeUp(300)
-//     $('#apple1').sildeUp(300)
-// }
-// $('.apple').on('click', function(event){
-//   const apple = $(event.target)
-//   apple.animate({"top":"+=100"},"fast")
-// });
+        const timer = setInterval(function(){ 
+            sec--;
+            $("#time").text(sec)
+            if(sec <= 0){
+                clearInterval(dropApple);
+                clearInterval(timer)
+                if(point >= 25){
+                   
+                }
+                else {
+                    
+                }
+            }
+        }, 1000);
+             
+            const dropApple = setInterval(function(){
+                const apple =$('.apple img')
+                apple.each(function(index,element){
+                //heightWindow
+                const heightWindow = $(window).height()
+                //heightRandom -100 to -200
+                const heightRandom =  Math.round(Math.random() * -100) - 100
+                
+                //widthRandom
+                    const widthWindow=$('#contanier').width()
+                    const widthRandom=Math.round(Math.random()* (widthWindow-10 ))
+                    // console.log(widthRandom)
+                    if (parseInt(element.style.top) > heightWindow){
+                        $(element).css({
+                            top:heightRandom,
+                            left:widthRandom
+                        })
 
-setInterval(function(){
-    const apple =$('.apple img')
-    
-// const random = appel.top.Math.random //TODO make random math 
-   
+                    }else{
+                        apple.animate({"top":"+=70"},"fast")
+                    }
+                    if( collision(element,$('#boy')) === true ){
+                        console.log('touched it (: ')
+                        gamePoint();
+                         
+                        
+                        
+                        
+            
+                    }
+                })
 
-    apple.each(function(index,element){
-     //heightWindow
-     const heightWindow = $(window).height()
-     //heightRandom -100 to -200
-     const heightRandom =  Math.round(Math.random() * -100) - 100
-    
-     //widthRandom
-        const widthWindow=$('#contanier').width()
-        const widthRandom=Math.round(Math.random()* (widthWindow-10 ))
-        // console.log(widthRandom)
-        if (parseInt(element.style.top) > heightWindow){
-            $(element).css({
-                top:heightRandom,
-                left:widthRandom
+
+            },400)
+
+            const playBoy=$('.boy');//this is the boy
+
+            $(document).keydown (function(e){ 
+                    if (e.which == '39'){
+                playBoy.finish().animate({"left":"+=50"},100)}
+                else if (e.which == '37'){
+                playBoy.finish().animate({"left":"-=50"},100)}
+
             })
 
-        }else{
-            apple.animate({"top":"+=80"},"fast")
-        }
-        if( collision(element,$('#boy')) === true ){
-            console.log('touched it (: ')
-        }
-    })
+            //select the left div
+            let leftArrow =$('#left')
+            
+            leftArrow.on('click',function(){
+                playBoy.finish().animate({"left":"-=50"},100)
+            })
 
 
-  },300)
+            //select the right div
+            let rightArrow =$('#right')
+            
+            rightArrow.on('click',function(){
+                playBoy.finish().animate({"left":"+=50"},100)
+            })
 
-const playBoy=$('.boy');
-$(document).keydown (function(e){ 
 
-    left=$(window).width()
 
-    if (e.which == '37'){
-        
-    playBoy.finish().animate({"left":"+=50"},100)}
-    else if (e.which == '39'){
-    playBoy.finish().animate({"left":"-=50"},100)}
-    // if($(boyT).position())
-        // console.log($(".boy").position())
-        // console.log($(".apple").eq(0))
- })
+            // function collision START HERE
+            function collision(firstElment,secondElement){
+                // console.log('collision starting...')
+                // First element
+                let x1 = $(firstElment).offset().left
+                let y1 = $(firstElment).offset().top
+                let h1 = $(firstElment).outerHeight(true);
+                let w1 = $(firstElment).outerWidth(true);
+                let b1 = y1 + h1;
+                let r1 = x1 + w1;
+                // Second element
+                let x2 = $(secondElement).offset().left
+                let y2 = $(secondElement).offset().top
+                let h2 = $(secondElement).outerHeight(true);
+                let w2 = $(secondElement).outerWidth(true);
+                let b2 = y2 + h2;
+                let r2 = x2 + w2;
+                if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2){
+                    return false
+                }
 
-// function collision START HERE
-function collision(firstElment,secondElement){
-    // console.log('collision starting...')
-    // First element
-    let x1 = $(firstElment).offset().left
-    let y1 = $(firstElment).offset().top
-    let h1 = $(firstElment).outerHeight(true);
-    let w1 = $(firstElment).outerWidth(true);
-    let b1 = y1 + h1;
-    let r1 = x1 + w1;
-    // Second element
-    let x2 = $(secondElement).offset().left
-    let y2 = $(secondElement).offset().top
-    let h2 = $(secondElement).outerHeight(true);
-    let w2 = $(secondElement).outerWidth(true);
-    let b2 = y2 + h2;
-    let r2 = x2 + w2;
-    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2){
-        return false
-    }
+                return true;
+            }
 
-    return true;
-}
+            const fElement = $('#apple1')
+            const sElement = $('#boy')
+            collision(fElement,sElement)
+            // function collision END HERE
 
-const fElement = $('#apple1')
-const sElement = $('#boy')
-collision(fElement,sElement)
-// function collision END HERE
+
+
+
+            function gamePoint(){
+            point++;
+            let pointE=$('#point');
+                pointE.text(point);
+            
+            }
+
+    } //THE GAME END HERE
+
+
+    setTimeout(startGame,1000)
+
+    
+})
 
 
 
